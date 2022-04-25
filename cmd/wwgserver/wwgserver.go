4e7aab4e4afe133e2234baac7e07dea7af449281
Stu Dobbie <studobster@gmail.com>
2022-04-25 16:35:20 +1000

    add aws sdk (#4)

--- cmd/wwgserver/wwgserver.go
+++ cmd/wwgserver/wwgserver.go
@@ -1,9 +1,15 @@
+// WWG Server
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 const (
@@ -12,8 +18,8 @@ const (
 )
 
 func main() {
-
 	http.HandleFunc("/", HelloWorldHandler)
+	http.HandleFunc("/buckets", BucketSearchHandler)
 
 	addr := fmt.Sprintf("%s:%d", hostname, port)
 	log.Printf("Starting server on %s\n", addr)
@@ -23,4 +29,30 @@ func main() {
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello world!!")
 	log.Println("The user called to say hello.")
+}
+
+func BucketSearchHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("The user is asking for bucket contents.")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("sydneyclimbing.com"),
+	})
+
+	if err != nil {
+		fmt.Fprintf(w, "error trying to listobjectsv2: %s", err)
+		return
+	}
+
+	for _, object := range output.Contents {
+		fmt.Fprintf(w, "key=%s size=%d\n", aws.ToString(object.Key), object.Size)
+	}
+
+	log.Printf("And we reported %d items.\n", len(output.Contents))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/wwg
 
 go 1.16
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.2 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.5 // indirect
+)
